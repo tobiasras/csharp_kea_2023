56b43b76e326ff0baf77a14517c760c5080839a2
tobiasras <tobiasras.job@gmail.com>
2023-08-24 18:16:43 +0200

    Finished delegat events task, with lamda and normal func

---
+++ Delegates_Events/CarEngine.cs
@@ -0,0 +1,32 @@
+﻿namespace Delegates_Events;
+
+class CarEngine
+{
+    public void StartEngine()
+    {
+        IsRunning = true;
+        EngineStarted.Invoke();
+    }
+
+    public void StopEngine()
+    {
+        IsRunning = false;
+        EngineStopped.Invoke();
+    }
+    public void SetEngineSpeed(int speed)
+    {
+        CurrentEngineSpeed = speed;
+        EngineSpeedChanged.Invoke(speed);
+    }
+
+    private bool IsRunning { get; set; } = false;
+    private int CurrentEngineSpeed { get; set; } = 0;
+
+    public delegate void EngineStartHandler();
+    public delegate void EngineStopHandler();
+    public delegate void EngineSpeedChangeHandler(int speed);
+
+    public event EngineStartHandler EngineStarted;
+    public event EngineStopHandler EngineStopped;
+    public event EngineSpeedChangeHandler EngineSpeedChanged = (int speed) => { };
+}
---
+++ Delegates_Events/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Delegates_Events;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            CarEngine carEngine = new CarEngine();
+            
+            carEngine.EngineStarted += () => Console.WriteLine("The engine has started!");
+            carEngine.EngineStopped += () => Console.WriteLine("The engine has stopped!");
+            carEngine.EngineSpeedChanged += OnSpeedChangeOne;
+
+            carEngine.StartEngine();
+            carEngine.SetEngineSpeed(519);
+            carEngine.StopEngine();
+        }
+
+        static void OnSpeedChangeOne(int speed)
+        {
+            Console.WriteLine($"OnSpeedChangeOne: Engine speed is now {speed}");
+        }
+        
+    }
