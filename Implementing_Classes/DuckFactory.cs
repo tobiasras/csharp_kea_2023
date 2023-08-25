8f9ceb0e99c4746e5d86fe4c344bad5b53c5a43f
tobiasras <tobiasras.job@gmail.com>
2023-08-25 10:58:13 +0200

    created better switch in Implementing classes task

--- Implementing_Classes/DuckFactory.cs
+++ Implementing_Classes/DuckFactory.cs
@@ -4,50 +4,41 @@ class DuckFactory
 {
     public Duck CreateDuck(string duckType)
     {
-        switch (duckType)
+        return duckType.ToLower() switch
         {
-            case "Mallard":
-                return new NaturalDuck()
-                {
-                    Name = "Mallard",
-                    Gender = "Male",
-                    CanFly = true,
-                    HasOrangeBeak = true,
-                    IsDomestic = true
-                };
-
-            case "Pekin":
-                return new NaturalDuck()
-                {
-                    Name = "Pekin",
-                    Gender = "Female",
-                    CanFly = false,
-                    HasOrangeBeak = false,
-                    IsDomestic = false
-                };
-
-            case "DonaldDuck":
-                return new CartoonDuck()
-                {
-                    IsMainCharactor = true,
-                    IsDomestic = true,
-                    Name = "Donald duck",
-                    Gender = "Male",
-                    OutfitColor = "blue"
-                };
-
-            case "DaisyDuck":
-                return new CartoonDuck()
-                {
-                    IsMainCharactor = false,
-                    IsDomestic = true,
-                    Name = "Daisy Duck",
-                    Gender = "Female",
-                    OutfitColor = "Pink"
-                };
-
-            default:
-                throw new ArgumentException("Invalid duck name");
-        }
+            "mallard" => new NaturalDuck()
+            {
+                Name = "Mallard",
+                Gender = "Male",
+                CanFly = true,
+                HasOrangeBeak = true,
+                IsDomestic = true
+            },
+            "pekin" => new NaturalDuck()
+            {
+                Name = "Pekin",
+                Gender = "Female",
+                CanFly = false,
+                HasOrangeBeak = false,
+                IsDomestic = false
+            },
+            "donaldduck" => new CartoonDuck()
+            {
+                IsMainCharactor = true,
+                IsDomestic = true,
+                Name = "Donald duck",
+                Gender = "Male",
+                OutfitColor = "blue"
+            },
+            "daisyduck" => new CartoonDuck()
+            {
+                IsMainCharactor = false,
+                IsDomestic = true,
+                Name = "Daisy Duck",
+                Gender = "Female",
+                OutfitColor = "Pink"
+            },
+            _ => throw new ArgumentException("Invalid duck name")
+        };
     }
 }
