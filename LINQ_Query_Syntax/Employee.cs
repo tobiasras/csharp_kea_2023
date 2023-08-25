d9f015794ca7137fcd9ecb1cfcafceab295e3fd3
tobiasras <tobiasras.job@gmail.com>
2023-08-25 06:43:41 +0200

    finished Linq Query syntax and refactoerd solution

---
+++ LINQ_Query_Syntax/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace LINQ_Query_Syntax;
+
+class Employee
+{
+    public string Name { get; set; } = string.Empty;
+    public string Department { get; set; } = string.Empty;
+    public decimal Salary { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Name} - {Department} - {Salary.ToString(CultureInfo.CurrentCulture)}";
+    }
+}
